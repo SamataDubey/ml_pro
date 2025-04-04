e0b18e306bc74ff16ca26dadaef29df4a699a56e
SamataDubey <samatadubey02@gmail.com>
2025-04-04 15:02:37 +0530

    utils file

--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -3,6 +3,35 @@ import sys
 from src.mlproject.exception import CustomException
 from src.mlproject.logger import logging
 import pandas as pd
+from dotenv import load_dotenv
+import pymysql
 
-#pymysql, mysql-connector-python
+load_dotenv()
+
+host=os.getenv("host")
+user=os.getenv("user")
+password=os.getenv("password")
+db=os.getenv("db")
+
+#pymysql, mysql-connector- pyhon , python-dotenv
+#environment variable
+
 def read_sql_data():
+    logging.info("Reading sql database started")
+    try:
+        mydb=pymysql.connect(
+            host=host,
+            user = user,
+            password = password,
+            db=db
+        )
+        logging.info("connection establised",mydb)
+        df=pd.read_sql_query('Select * from students',mydb)
+        print(df.head)
+
+        return df
+
+
+
+    except Exception as ex:
+        raise CustomException(ex)
