74a4311bf807d6bd296919698b06e509da183efa
SamataDubey <samatadubey02@gmail.com>
2025-04-04 14:58:39 +0530

    data ingestion

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
+from src.mlproject.components.data_ingestion import DataIngestion
+from src.mlproject.components.data_ingestion import DataIngestionConfig
+
 import sys
 
 if __name__ =="__main__":
@@ -7,7 +10,10 @@ if __name__ =="__main__":
     logging.info("the execution has started")
 
     try:
-        a=1/0
+        # data_ingestion= DataIngestionConfig
+        data_ingestion = DataIngestion
+        data_ingestion.initiate_data_ingestion()
+        
 
     except Exception as e:
         logging.info("Custom Exception")
---
+++ src/mlproject/utils.py
@@ -0,0 +1,8 @@
+import os
+import sys
+from src.mlproject.exception import CustomException
+from src.mlproject.logger import logging
+import pandas as pd
+
+#pymysql, mysql-connector-python
+def read_sql_data():
